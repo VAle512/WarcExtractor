b0a3133c3c210718e46fcf58b4a5063bdfdaaee9
VAle512 <valerio_cetorelli@yahoo.it>
2017-09-15 17:03:19 +0200

    1st tested and working version

--- src/io/github/vale512/warcExtractor/exceptions/DirectoryAlreadyExistsException.java
+++ src/io/github/vale512/warcExtractor/exceptions/DirectoryAlreadyExistsException.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2017 Valerio Cetorelli (valerio_cetorelli@yahoo.it)
+ * 
+ * This file is part of WarcExtractor.
+ *
+ *   WarcExtractor is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   WarcExtractor is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with WarcExtractor.  If not, see <http://www.gnu.org/licenses/>
+ *   
+ *   @author Valerio Cetorelli (valerio_cetorelli@yahoo.it)
+ */
 package io.github.vale512.warcExtractor.exceptions;
 
 import java.nio.file.FileAlreadyExistsException;
